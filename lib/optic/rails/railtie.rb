1cbe82bce40992f92880197f3f37cc956ad646db
Bradley Buda <bradleybuda@gmail.com>
2018-05-21 11:28:10 -0700

    Add supervisor and worker threads, improve logging

--- lib/optic/rails/railtie.rb
+++ lib/optic/rails/railtie.rb
@@ -1,37 +1,81 @@
+require "action_cable_client"
+require "eventmachine"
+
 module Optic
   module Rails
     class Railtie < ::Rails::Railtie
       initializer "optic_rails.launch_client_thread" do |app|
+        # TODO use tagged logger, make it clear which thread log messages are coming from
+
+        # Get configuration
         api_key = app.config.optic_api_key # TODO fail gracefully if missing
         uri = app.config.optic_uri
-        puts "Launching Optic client thread"
-        Thread.new do
 
-          EventMachine.run do
-            puts "connecting"
-            client = ActionCableClient.new(uri, { channel: "MetricsChannel" }, true, { "Authorization" => "Bearer #{api_key}" })
-            client.connected do
-              puts "successfully connected"
-            end
+        logger = ::Rails.logger
 
-            client.errored { |msg| puts "ERROR: #{msg}" }
+        logger.debug "Starting supervisor thread"
+        supervisor = Thread.new do
+          loop do
+            logger.debug "Starting worker thread"
+            worker = Thread.new do
+              EventMachine.run do
+                client = ActionCableClient.new(uri, { channel: "MetricsChannel" }, true, { "Authorization" => "Bearer #{api_key}" })
 
-            # called whenever a message is received from the server
-            client.received do |message|
-              puts "MESSAGE: #{message}"
-              command = message["message"]["command"]
+                client.connected do
+                  logger.debug "Optic agent connected"
+                end
 
-              case command
-              when "request_schema"
-                puts "Schema requested!"
-                client.perform "schema", message: Optic::Rails.get_entities
-              when "request_metrics"
-                puts "Metrics requested!"
-                client.perform "metrics", message: Optic::Rails.get_metrics(message["message"]["pivot"])
-              else
-                puts "unknown command!"
+                client.disconnected do
+                  logger.warn "Optic agent disconnected, killing client thread"
+                  EventMachine.stop
+                end
+
+                client.errored do |msg|
+                  logger.warn "Optic agent error: #{msg}"
+                  EventMachine.stop
+                  # TODO stop worker?
+                end
+
+                client.subscribed do
+                  logger.debug "Optic agent subscribed"
+                end
+
+                client.pinged do |msg|
+                  logger.debug "Optic agent pinged: #{msg}"
+                end
+
+                # called whenever a message is received from the server
+                client.received do |message|
+                  logger.debug "Optic agent received: #{message}"
+                  command = message["message"]["command"]
+
+                  case command
+                  when "request_schema"
+                    logger.debug "Optic agent got schema request"
+                    client.perform "schema", message: Optic::Rails.get_entities
+                    logger.debug "Optic agent sent schema"
+                  when "request_metrics"
+                    logger.debug "Optic agent got metrics request"
+                    client.perform "metrics", message: Optic::Rails.get_metrics(message["message"]["pivot"])
+                    logger.debug "Optic agent sent metrics"
+                  else
+                    logger.warn "Optic agent got unknown command: #{command}"
+                  end
+                end
               end
+
+              logger.warn "Stopping worker thread"
             end
+
+            begin
+              worker.join
+            rescue => e
+              logger.error "Worker thread died with error", e
+            end
+
+            logger.warn "Supervisor thread detected dead worker, sleeping"
+            sleep 5.0
+            logger.debug "Supervisor thread done sleeping"
           end
         end
       end
--- optic-rails.gemspec
+++ optic-rails.gemspec
@@ -16,8 +16,10 @@ Gem::Specification.new do |s|
 
   s.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
 
-  s.add_dependency "rails", "~> 5.2.0.rc2"
-  s.add_dependency "rgl", "~> 0.5.3"
+  s.add_runtime_dependency "action_cable_client", "~> 2.0.2"
+  s.add_runtime_dependency "eventmachine", "~> 1.0"
+  s.add_runtime_dependency "rails", "~> 5.2.0.rc2"
+  s.add_runtime_dependency "rgl", "~> 0.5.3"
 
   s.add_development_dependency "sqlite3"
 end
